07f5db057b351e5521523870fc984caed72979c3
yuanmei.Chen <2521722246@qq.com>
2018-01-06 18:55:37 +0800

    添加请求文件

---
+++ SourceCode/src/server/apiModel/RequestAjax.js
@@ -0,0 +1,30 @@
+import axios from 'axios'
+import querystring from 'qs'
+
+if (!window.Promise) {
+  window.Promise = Promise
+}
+// 添加一个请求拦截器
+axios.interceptors.request.use(function (config) {
+    // 在请求发出之前进行一些操作
+  return config
+}, function (err) {
+    // Do something with request error
+  return Promise.reject(err)
+})
+  // 添加一个响应拦截器
+axios.interceptors.response.use(function (res) {
+    // 在这里对返回的数据进行处理
+  return res
+}, function (err) {
+    // Do something with response error
+  return Promise.reject(err)
+})
+
+export const httpFetch = (url, params) => {
+  axios.post(url, querystring.stringify(params)).then((response) => {
+    Promise.resolve(response.data)
+  }).catch((error) => {
+    Promise.reject(error)
+  })
+}
