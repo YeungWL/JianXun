aba26c27c1c0e9c6082336fdc021ee2c6b072a41
liexin.zhang@g-emall.com <liexin.zhang@g-emall.com>
2018-01-10 17:12:45 +0800

    换头像

--- SourceCode/src/components/Sidebar.vue
+++ SourceCode/src/components/Sidebar.vue
@@ -12,7 +12,7 @@
                 <!-- menu profile quick info -->
                 <div class="profile clearfix">
                     <div class="profile_pic">
-                        <img src="../assets/images/img.jpg" alt="..." class="img-circle profile_img">
+                        <img src="../assets/images/user.png" alt="..." class="img-circle profile_img">
                     </div>
                     <div class="profile_info">
                         <span>Welcome,</span>
--- SourceCode/src/components/TopNav.vue
+++ SourceCode/src/components/TopNav.vue
@@ -12,7 +12,7 @@
                     <ul class="nav navbar-nav navbar-right">
                         <li class="">
                             <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                                <img src="../assets/images/img.jpg" alt="">{{userInfo.username}}
+                                <img src="../assets/images/user.png" alt="">{{userInfo.username}}
                                 <span class=" fa fa-angle-down"></span>
                             </a>
                             <ul class="dropdown-menu dropdown-usermenu pull-right">
