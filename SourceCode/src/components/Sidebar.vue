f6f3e3ba3cdb9b1240d326e30c737243ecf7e498
yuanmei.Chen <2521722246@qq.com>
2018-01-10 19:05:39 +0800

    打包数据

--- SourceCode/src/components/Sidebar.vue
+++ SourceCode/src/components/Sidebar.vue
@@ -15,8 +15,8 @@
                         <img src="../assets/images/user.png" alt="..." class="img-circle profile_img">
                     </div>
                     <div class="profile_info">
-                        <span>Welcome,</span>
-                        <h2>John Doe</h2>
+                        <span>欢迎回来,</span>
+                        <h2>{{userInfo.username}}</h2>
                     </div>
                 </div>
                 <!-- /menu profile quick info -->
@@ -45,3 +45,17 @@
        background: rgba(255,255,255,.05);
    }
 </style>
+<script>
+import {mapGetters} from 'vuex'
+export default{
+  created () {
+    // 获取用户信息
+    this.$store.commit('GETUSERINFO')
+  },
+  computed: {
+    ...mapGetters([
+      'userInfo'
+    ])
+  }
+}
+</script>
