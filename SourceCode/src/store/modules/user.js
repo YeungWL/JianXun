8c17c8ff647fabd01ccf75f86dc06d4e03a11c8c
liexin.zhang@g-emall.com <liexin.zhang@g-emall.com>
2018-01-11 18:06:08 +0800

    修改登录判断

--- SourceCode/src/store/modules/user.js
+++ SourceCode/src/store/modules/user.js
@@ -35,30 +35,35 @@ const mutations = {
 
   // 用户登录
   [types.LOGIN] (state, obj) {
-    let loginType = Func.validateAccount(state.loginInfo.userAccount)
-    let params = {
-      LoginInfo: loginType + '\t' + state.loginInfo.userAccount + '\t' + state.loginInfo.password + '\t' + ConstVariable.clientType,
-      DeviceIdentity: '',
-      ClientSource: ConstVariable.clientSource
-    }
-    ConstVariable.HttpRequestAjax(RequestUrl.login, params).then((res) => {
-      if (res.isSuccess === ConstVariable.isSuccess && res.code === '1000') {
-        state.userInfo.username = res.resData.userName || ''
-        // 针对用户一些重要信息使用RSA加密
-        // res.resData.uid = ConstVariable.Utils.localRsaEncrypt(res.resData.uid)
-        // res.resData.accountUid = ConstVariable.Utils.localRsaEncrypt(res.resData.accountUid)
-        // res.resData.mobile = ConstVariable.Utils.localRsaEncrypt(res.resData.mobile)
-        // res.resData.userName = ConstVariable.Utils.localRsaEncrypt(res.resData.userName)
-        // res.resData.code = ConstVariable.Utils.localRsaEncrypt(res.resData.code)
-
-        // 存储用户信息
-        sessionStorage.setItem(ConstVariable.Utils.stringToBase64('userInfo'), ConstVariable.Utils.stringToBase64(JSON.stringify(res.resData)))
-
-        ConstVariable.router.push('/home')
-      } else {
-        Toast(res.description)
+    if (state.loginInfo.userAccount === '' || state.loginInfo.password === '') {
+      Toast('账号或者密码不能为空')
+    } else {
+      let loginType = Func.validateAccount(state.loginInfo.userAccount)
+      let params = {
+        LoginInfo: loginType + '\t' + state.loginInfo.userAccount + '\t' + state.loginInfo.password + '\t' + ConstVariable.clientType,
+        DeviceIdentity: '',
+        ClientSource: ConstVariable.clientSource
       }
-    })
+      // console.log(state)
+      ConstVariable.HttpRequestAjax(RequestUrl.login, params).then((res) => {
+        if (res.isSuccess === ConstVariable.isSuccess && res.code === '1000') {
+          state.userInfo.username = res.resData.userName || ''
+          // 针对用户一些重要信息使用RSA加密
+          // res.resData.uid = ConstVariable.Utils.localRsaEncrypt(res.resData.uid)
+          // res.resData.accountUid = ConstVariable.Utils.localRsaEncrypt(res.resData.accountUid)
+          // res.resData.mobile = ConstVariable.Utils.localRsaEncrypt(res.resData.mobile)
+          // res.resData.userName = ConstVariable.Utils.localRsaEncrypt(res.resData.userName)
+          // res.resData.code = ConstVariable.Utils.localRsaEncrypt(res.resData.code)
+
+          // 存储用户信息
+          sessionStorage.setItem(ConstVariable.Utils.stringToBase64('userInfo'), ConstVariable.Utils.stringToBase64(JSON.stringify(res.resData)))
+
+          ConstVariable.router.push('/home')
+        } else {
+          Toast(res.description)
+        }
+      })
+    }
   },
 
   // 用户登出
